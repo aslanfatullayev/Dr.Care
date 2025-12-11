5536ea04c8e6cfe0937605f72aebf2d569aba4e4
aslanfatullayev <aslanfatullev2006@gmail.com>
2025-12-11 15:55:37 +0500

    model.py  update

--- base/models.py
+++ base/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to='news_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_DIR = BASE_DIR / 'media'
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
